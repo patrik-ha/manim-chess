f414dcde5d007e3716c17041a747ce159224429f
patrih <patrikham0@gmail.com>
2023-02-14 11:08:00 +0100

    Continuing (needs to look better)

--- chess.py
+++ chess.py
@@ -1,36 +1,65 @@
 from manim import *
 
-def piece_to_icon(c):
+def piece_to_icon(c, fill=True):
     color = RED if c.isupper() else BLACK
+    prefix = r"\Black" if c.isupper() else r"\Black"
+    prefix = prefix if not c.isspace() else ""
     lookup = {
-        'k': r"\symking",
-        'q': r"\symqueen",
-        'r': r"\symrook",
-        'b': r"\symbishop",
-        'n': r"\symknight",
-        'p': r"\sympawn",
+        'k': "King",
+        'q': "Queen",
+        'r': "Rook",
+        'b': "Bishop",
+        'n': "Knight",
+        'p': "Pawn",
+        ' ': ""
     }
+
+    suffix = "OnWhite" if c.isupper() else "OnWhite"
+    suffix = suffix if not c.isspace() else ""
+
     template = TexTemplate()
     template.add_to_preamble(r"\usepackage{skak}")
     tex = Tex(
-        lookup[c.lower()],
+        prefix + lookup[c.lower()] + suffix,
         tex_template=template,
-        color=color
+        color=color,
     )
-    tex.scale(1.9)
+
+    tex.set_fill(color)
+    tex.scale(1)
     return tex
 
+def clean_fen(fen):
+    # Only care about the position
+    # Also assume that the fen is valid or whatever
+    if " " in fen:
+        fen = fen.split(" ")[0]
+    
+    rows = fen.split("/")
+
+    for i, row in enumerate(rows):
+        new_row = ""
+        for c in row:
+            if c in "12345678":
+                new_row += " " * int(c)
+            else:
+                new_row += c
+        rows[i] = new_row
+    print(rows)
+    return rows
+
+
 def get_board(dims, fen):
+    fen = clean_fen(fen)
     template = TexTemplate()
     template.add_to_preamble(r"\usepackage{skak}")
-
     board = []
     for i in range(dims[0]):
         for j in range(dims[1]):
             color = WHITE if ((i + j) % 2) == 0 else BLUE
             square = Square(1, stroke_color=BLACK, stroke_width=DEFAULT_STROKE_WIDTH * 0.3)
             square.set_fill(color, 1)
-            icon = piece_to_icon("Q")
+            icon = piece_to_icon(fen[i][j])
             full_square = Group(square, icon)
             full_square.shift(i * DOWN + j * RIGHT)
             board.append(full_square)
--- scene.py
+++ scene.py
@@ -3,6 +3,6 @@ from chess import get_board
 
 class Chess(Scene):
     def construct(self):
-        board = get_board((5, 4), "")
+        board = get_board((5, 4), "rqkr/pppp/4/PPPP/RQKR w - - 0 1")
         self.add(board)
 
